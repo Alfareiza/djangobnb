3657594b720d42f319fc667ca52223b41007c205
Alfonso <alfareiza@gmail.com>
2024-03-18 20:23:37 -0300

    Created next.js project and establish some basic components

---
+++ djangobnb/app/components/navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import SearchFilters from './SearchFilters';
+
+const Navbar = () => {
+    return (
+    <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
+        <div className="max-w-[100%] max-auto px-6">
+            <div className="flex justify-between items-center">
+                <Link href="/">
+                    <Image src="/logo.png" alt="DjangoBnb logo" width={150} height={38} />
+                </Link>
+
+                <div className="flex space-x-6">
+                    <SearchFilters/>
+                </div>
+
+                <div className="flex items-center space-x-6">
+                    <div>
+                    Add Property  - User Nav
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    )
+}
+
+export default Navbar;
---
+++ djangobnb/app/components/navbar/SearchFilters.tsx
@@ -0,0 +1,30 @@
+const SearchFilters = () => {
+    return (
+            <div className="h-[64px] flex flex-row items-center justify-between border rounded-full">
+                <div>
+                    <div className="flex flex-row items-center justify-between">
+                        <div className="w-[250px] h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+                            <p className="text-xs font-semibold">Check in</p>
+                            <p className="text-sm">Add dates</p>
+                        </div>
+                        <div className="h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+                            <p className="text-xs font-semibold">Check in</p>
+                            <p className="text-sm">Add dates</p>
+                        </div>
+                        <div className="h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+                            <p className="text-xs font-semibold">Check in</p>
+                            <p className="text-sm">Add dates</p>
+                        </div>
+                        <div className="h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+                            <p className="text-xs font-semibold">Who</p>
+                            <p className="text-sm">Add guests</p>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+    )
+}
+
+export default SearchFilters;
