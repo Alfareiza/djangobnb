44670764eda71473e7b9cf9ac9b6971bd61d0fa1
Alfonso <alfareiza@gmail.com>
2024-03-19 13:19:38 -0300

    Defining front of AddPropertyButton component plus fixing general details in front to make it responsive

---
+++ djangobnb/app/components/navbar/AddPropertyButton.tsx
@@ -0,0 +1,9 @@
+const AddPropertyButton = () => {
+    return (
+            <div className="cursor-pointer px-4 py-3 text-sm font-semibold rounded-full hover:bg-gray-100">
+                    Add Property
+            </div>
+    )
+}
+
+export default AddPropertyButton;
