d67ef08cf67039e622485e63ba241bf6ca4ba59c
Alfonso <alfareiza@gmail.com>
2024-03-22 10:42:47 -0300

    Integrated backend with frontent and showing properties from DRF to frontend

--- djangobnb/app/components/properties/PropertyList.tsx
+++ djangobnb/app/components/properties/PropertyList.tsx
@@ -1,13 +1,52 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
+import {useEffect, useState} from 'react';
 import PropertyListItem from "./PropertyListItem";
 
+export type PropertyType = {
+    id: string;
+    image_url: string;
+    price_per_night: number;
+    title: string;
+}
+
 const PropertyList = () => {
+
+    const [properties, setProperties] = useState<PropertyType[]>([]);
+
+    const getProperties = async () => {
+        const url = 'http://localhost:8000/api/properties/'
+        await fetch(url, {
+                method: 'GET'
+            })
+                .then(response => response.json())
+                .then((json) => {
+                    console.log('json', json);
+                    setProperties(json.data)
+                })
+                .catch((error) => {
+                    console.log('json', error);
+                })
+    }
+
+    useEffect (() => {
+        getProperties();
+    }, []);
+
     return (
             <>
-                <PropertyListItem/>
-                <PropertyListItem/>
-                <PropertyListItem/>
+                { properties.map((property) => {
+                console.log(property)
+                        return (
+                            <PropertyListItem
+                                key={property.id}
+                                property={property}
+                            />
+                        )
+                    }
+                )}
             </>
     )
 }
--- djangobnb/app/components/properties/PropertyListItem.tsx
+++ djangobnb/app/components/properties/PropertyListItem.tsx
@@ -1,23 +1,29 @@
 import Image from "next/image";
-import Link from "next/link";
+import PropertyType from './PropertyList';
 
-const PropertyListItem = () => {
+interface PropertyProps {
+    property: PropertyType; // PropertyType is the model of Property in Typescript environment
+}
+
+const PropertyListItem: React.FC<PropertyProps> = ({
+    property
+}) => {
     return (
             <div className="cursor-pointer">
                 <div className="relative overflow-hidden aspect-square rounded-xl">
                     <Image
                         fill
-                        src="/props/prop-beachfront-a1.png"
+                        src={property.image_url}
                         sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
                         className="hover:scale-110 object-cover transition h-full w-full"
-                        alt="Beach House"
+                        alt="`{property.title}``"
                     />
                 </div>
                 <div className="mt-2">
-                    <p className='text-lg font-bold'> North Bay village, Florida</p>
+                    <p className='text-lg font-bold'>{property.title}</p>
                 </div>
                 <div className="mt-2">
-                    <p className='text-sm text-gray-500'> <b>$200</b>, night</p>
+                    <p className='text-sm text-gray-500'> <b>${property.price_per_night}</b>, night</p>
                 </div>
             </div>
     )
