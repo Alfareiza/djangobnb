b0171ae0b0c5017c5ec16f2ad8b64f53155d2a59
Alfonso <alfareiza@gmail.com>
2024-03-22 11:44:11 -0300

    Created POST method

--- djangobnb/app/services/apiService.ts
+++ djangobnb/app/services/apiService.ts
@@ -1,7 +1,7 @@
 // Responsible for communication with API
 const apiService = {
     get: async function (url: string): Promise<any> {
-        console.log('get', url);
+        console.log('Obtaining data [GET] from ', url);
 
         return new Promise((resolve, reject) =>{
             fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
@@ -13,13 +13,34 @@ const apiService = {
             })
                 .then(response => response.json())
                 .then((json) => {
-                    console.log('Reponse ->', json);
+                    console.log('Response ->', json);
                     resolve(json);
                 })
                 .catch((error) => {
                     reject(error);
                 })
         })
-    }
+    },
+
+   post: async function(url: string, data: any): Promise<any>{
+        console.log('Requesting data [GET] to ', url);
+        return new Promise((resolve, reject) =>{
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then((json) => {
+                    console.log('Response ->', json);
+                    resolve(json);
+                })
+                .catch((error) => {
+                    reject(error);
+                })
+        })
+   }
 }
 export default apiService;
